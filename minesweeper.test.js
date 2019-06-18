d20a092586b6c797932bad2c59a8c282b34b8053
Michael Cheng <mcheng.work@gmail.com>
2019-06-18 16:43:11 +0800

    Change to table test.

--- minesweeper.test.js
+++ minesweeper.test.js
@@ -1,46 +1,44 @@
 import mineSweeper from './minesweeper'
 
-test('Empty input returns empty response', () => {
-  const result = mineSweeper(0, 0, [])
-  const expected = []
-
-  expect(result).toEqual(expected)
-})
-
-test('Empty grid with no mines', () => {
-  const result = mineSweeper(3, 2, [])
-  const expected = [
-    '...',
-    '...'
+const cases = [
+  ['Empty input returns empty response', 0, 0, [], []],
+  [
+    'Empty grid with no mines', 3, 2, [],
+    [
+      '...',
+      '...'
+    ]
+  ],
+  [
+    '3 x 2 grid with 1 mine', 3, 2,
+    [
+      '.*.',
+      '...'
+    ],
+    [
+      '1*1',
+      '111'
+    ]
+  ],
+  [
+    '5 x 3 grid with 3 mine', 5, 3,
+    [
+      '**...',
+      '.....',
+      '.*...'
+    ],
+    [
+      '**1..',
+      '332..',
+      '1*1..'
+    ]
   ]
+]
 
-  expect(result).toEqual(expected)
-})
-
-test('3 x 2 grid with 1 mine', () => {
-  const result = mineSweeper(3, 2, [
-    '.*.',
-    '...'
-  ])
-  const expected = [
-    '1*1',
-    '111'
-  ]
-
-  expect(result).toEqual(expected)
-})
-
-test('5 x 3 grid with 3 mine', () => {
-  const result = mineSweeper(5, 3, [
-    '**...',
-    '.....',
-    '.*...'
-  ])
-  const expected = [
-    '**1..',
-    '332..',
-    '1*1..'
-  ]
-
-  expect(result).toEqual(expected)
-})
+test.each(cases)(
+  'mineSweeper() - %s',
+  (_, cols, rows, mines, expected) => {
+    const result = mineSweeper(cols, rows, mines)
+    expect(result).toEqual(expected)
+  }
+)
